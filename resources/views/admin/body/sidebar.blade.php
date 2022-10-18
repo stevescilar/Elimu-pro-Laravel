7af24761098a3b5cfc3ce4afcf679d6d12d3a0fe
stevescilar <stevemuambi@gmail.com>
2022-10-18 15:28:25 +0300

    sidebar prefix

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,11 @@
+@php
+    $prefix = Request::route()->getPrefix();
+    $route  = Route::current()->getName();
+
+@endphp
+
+
+
 <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">	
@@ -17,14 +25,14 @@
       <!-- sidebar menu-->
       <ul class="sidebar-menu" data-widget="tree">  
 		  
-		<li>
+		<li class="{{ ($route == 'dashboard')?'active':'' }}">
           <a href="{{route('dashboard')}}">
             <i data-feather="pie-chart"></i>
 			<span>Dashboard</span>
           </a>
         </li>  
 		
-        <li class="treeview">
+        <li class="treeview {{ ($prefix == '/users')?'active':'' }}">
           <a href="#">
             <i data-feather="users"></i>
             <span>Manage User</span>
@@ -38,7 +46,7 @@
           </ul>
         </li> 
 		  
-        <li class="treeview">
+        <li class="treeview {{ ($prefix == '/profile')?'active':'' }}">
           <a href="#">
             <i data-feather="settings"></i> <span>Manage Profile</span>
             <span class="pull-right-container">
