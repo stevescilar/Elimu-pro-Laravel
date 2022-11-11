6fb9dc4976f9ac482f07a8d638577ead5cbb5d84
stevescilar <stevemuambi@gmail.com>
2022-11-11 14:54:39 +0300

    Subjects-roundup

--- resources/views/backend/setup/group/add_group.blade.php
+++ resources/views/backend/setup/group/add_group.blade.php
@@ -6,13 +6,13 @@
         <div class="content-header">
             <div class="d-flex align-items-center">
                 <div class="mr-auto">
-                    <h3 class="page-title">Add Group</h3>
+                    <h3 class="page-title">Add Subjects</h3>
                     <div class="d-inline-block align-items-center">
                         <nav>
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
-                                <li class="breadcrumb-item" aria-current="page">Groups</li>
-                                <li class="breadcrumb-item active" aria-current="page">Add Student Group</li>
+                                <li class="breadcrumb-item" aria-current="page">Subjects</li>
+                                <li class="breadcrumb-item active" aria-current="page">Add Student</li>
                             </ol>
                         </nav>
                     </div>
