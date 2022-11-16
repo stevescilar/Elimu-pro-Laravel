abe3f985c1405a23c0c7e6b9dba52aee9e929330
stevescilar <stevemuambi@gmail.com>
2022-11-16 13:05:22 +0300

    relationships

--- app/Models/FeeAmount.php
+++ app/Models/FeeAmount.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class FeeAmount extends Model
 {
     public function fee_category(){
+        // creating a relationship
         return $this->belongsTo(FeeCategory::class,'fee_category_id','id');
     }
 }
